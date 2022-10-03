fdab29d5ed4a397ccd9aadfc269a8a6ce7058b2d
Sabur Ahmed <saburahmed794@gmail.com>
2022-10-03 09:56:28 +0100

    add Jest config

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,14 @@ import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
+import Home from "../src/screens/Home";
 
-test("renders learn react link", () => {
+test("renders form button", () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
+      <Home />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/locate/i)).toBeInTheDocument();
 });
